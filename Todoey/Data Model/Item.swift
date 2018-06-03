6728cf4521f43fde58bc55c08c215854ac1204b7
Jacob <jacob@Supers-MacBook-Pro.local>
2018-06-03 19:14:33 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jacob on 03/06/2018.
+//  Copyright © 2018 Jacob Ujanga. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done:  Bool = false
+}
